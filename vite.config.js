a768775a1d8c27b37bc070090e4cedb76a957c9e
2014melvil <melvil.yvoncleaud@gmail.com>
2025-11-16 13:15:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // Dit à Vite que la racine est ici
+  build: {
+    outDir: 'dist',
+  }
+})
